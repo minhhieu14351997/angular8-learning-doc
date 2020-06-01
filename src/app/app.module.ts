3a79893e682eafdfd434b7e4d0ca43d92a2afc1e
minhhieu14351997 <40237041+minhhieu14351997@users.noreply.github.com>
2020-06-01 20:07:14 +0700

    test webstorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,9 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
